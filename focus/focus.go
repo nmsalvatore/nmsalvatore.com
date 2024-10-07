e7132bcdae3cc1a1cad05f3ccfc718380811a1ca
Nick Salvatore <nmsalvatore@gmail.com>
2024-10-06 19:59:02 -0700

    feat: initial commit of working focus randomizer

---
+++ focus/focus.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"os"
+)
+
+type Focus struct {
+	options Options
+}
+
+func (f Focus) Work() string {
+	return RandomOption(f.options.Type.Work)
+}
+
+func (f Focus) Movement() string {
+	return RandomOption(f.options.Type.Movement)
+}
+
+type Options struct {
+	Type struct {
+		Work     []string `json:"work"`
+		Movement []string `json:"movement"`
+	} `json:"options"`
+}
+
+type Result struct {
+	Work     string `json:"work"`
+	Movement string `json:"movement"`
+}
+
+func main() {
+	options := ReadOptionsAsJSON()
+	focus := Focus{options}
+	result := Result{
+		focus.Work(),
+		focus.Movement(),
+	}
+	WriteResultsAsJSON(result)
+}
+
+func ReadOptionsAsJSON() Options {
+	contents, err := os.ReadFile("./data/options.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var options Options
+	err = json.Unmarshal(contents, &options)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return options
+}
+
+func RandomOption(options []string) string {
+	return options[rand.Intn(len(options))]
+}
+
+func WriteResultsAsJSON(result Result) {
+	data, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.WriteFile("./data/results.json", data, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ focus/go.mod
@@ -0,0 +1,3 @@
+module github.com/nmsalvatore/nmsalvatore.com
+
+go 1.23.1
