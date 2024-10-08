0a1b20a9d40456d3c00fad2ff4e937796392642a
Nick Salvatore <nmsalvatore@gmail.com>
2024-10-07 20:45:10 -0700

    feat: noscript text and render js view after js loaded

--- public/app.js
+++ public/app.js
@@ -1,13 +1,21 @@
 const work = document.getElementById("work");
 const movement = document.getElementById("movement");
 
-(async () => {
+async function renderFocus() {
     try {
         const response = await fetch("results.json");
+        if (!response.ok) {
+            throw new Error("no response");
+        }
         const data = await response.json();
-        work.innerText = data.work;
-        movement.innerText = data.movement;
+        work.innerText = await data.work;
+        movement.innerText = await data.movement;
     } catch (err) {
         console.error("Could not fetch data:", err);
     }
-})();
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    await renderFocus();
+    document.querySelector("main.content").style.display = "block";
+});
